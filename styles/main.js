66aa555e513e5d16792602c8307d7190e04813ed
bepin-jenkins <jenkins_bepinex@protonmail.com>
2019-12-03 20:13:53 +0000

    Update docs

--- styles/main.js
+++ styles/main.js
@@ -1,1 +1,12 @@
-// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
+$(function() {
+    $.ajax("/versions.json", { dataType: "json" })
+        .done(function(data) {
+            $("select.versions-dropdown")
+                .append(data.versions.map(function(v) { return $("<option/>", { value: v.tag, text: v.name }); }))
+                .val(location.pathname.split("/")[1])
+                .on("change", function(evt) {
+                    location.href = location.origin + "/" + evt.target.value + "/"
+                });
+            $(".version-selector").show();
+        });
+});
