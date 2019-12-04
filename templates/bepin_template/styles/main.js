baa5cbdb476f5747bdd829a2ffc58112a2dac668
ghorsington <neigh@coder.horse>
2019-12-04 21:41:28 +0200

    Add url prefix support for versioning

--- templates/bepin_template/styles/main.js
+++ templates/bepin_template/styles/main.js
@@ -3,7 +3,7 @@ $(function() {
         .done(function(data) {
             $("select.versions-dropdown")
                 .append(data.versions.map(function(v) { return $("<option/>", { value: v.tag, text: v.name }); }))
-                .val(location.pathname.split("/")[1])
+                .val(location.pathname.substring(data.urlPrefix.length).split("/")[0])
                 .on("change", function(evt) {
                     location.href = location.origin + "/" + evt.target.value + "/"
                 });
