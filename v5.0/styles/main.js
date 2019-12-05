009c6656d2caec4e8308de11a849704feead7e72
denikson <geoffrey.hoooooorse@gmail.com>
2019-12-05 18:55:49 +0000

    Update gh-pages to output generated at b783e54

---
+++ v5.0/styles/main.js
@@ -0,0 +1,13 @@
+$(function() {
+    var urlPrefix = $("meta[property='docfx:urlPrefix']").attr("content");
+    $.ajax(urlPrefix + "versions.json", { dataType: "json" })
+        .done(function(data) {
+            $("select.versions-dropdown")
+                .append(data.versions.map(function(v) { return $("<option/>", { value: v.tag, text: v.name }); }))
+                .val(location.pathname.substring(urlPrefix.length).split("/")[0])
+                .on("change", function(evt) {
+                    location.href = location.origin + "/" + evt.target.value + "/"
+                });
+            $(".version-selector").show();
+        });
+});
