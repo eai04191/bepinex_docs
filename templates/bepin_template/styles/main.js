e41eccdf6e2a670576af1b58480b306de7cde7a7
ghorsington <neigh@coder.horse>
2019-12-05 18:56:42 +0200

    Fix version display script

--- templates/bepin_template/styles/main.js
+++ templates/bepin_template/styles/main.js
@@ -1,9 +1,10 @@
 $(function() {
-    $.ajax("/versions.json", { dataType: "json" })
+    var urlPrefix = $("meta[property='docfx:urlPrefix']").attr("content");
+    $.ajax(urlPrefix + "versions.json", { dataType: "json" })
         .done(function(data) {
             $("select.versions-dropdown")
                 .append(data.versions.map(function(v) { return $("<option/>", { value: v.tag, text: v.name }); }))
-                .val(location.pathname.substring(data.urlPrefix.length).split("/")[0])
+                .val(location.pathname.substring(urlPrefix.length).split("/")[0])
                 .on("change", function(evt) {
                     location.href = location.origin + "/" + evt.target.value + "/"
                 });
